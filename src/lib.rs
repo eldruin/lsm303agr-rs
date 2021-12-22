5109986045eddb96e98d2975c1af3650c225fe76
Zachary <me@dusterthefirst.com>
2021-12-22 09:49:41 +0100

    fix bad formatting

--- src/lib.rs
+++ src/lib.rs
@@ -81,10 +81,10 @@
 //!
 //! let dev = I2cdev::new("/dev/i2c-1").unwrap();
 //! let mut sensor = Lsm303agr::new_with_i2c(dev);
-//! 
+//!
 //! sensor.init().unwrap();
 //! sensor.set_accel_odr(AccelOutputDataRate::Hz10).unwrap();
-//! 
+//!
 //! loop {
 //!     if sensor.accel_status().unwrap().xyz_new_data {
 //!         let data = sensor.accel_data().unwrap();
@@ -108,10 +108,10 @@
 //! let accel_cs = Pin::new(17);
 //! let mag_cs = Pin::new(27);
 //! let mut sensor = Lsm303agr::new_with_spi(dev, accel_cs, mag_cs);
-//! 
+//!
 //! sensor.init().unwrap();
 //! sensor.set_accel_odr(AccelOutputDataRate::Hz10).unwrap();
-//! 
+//!
 //! loop {
 //!     if sensor.accel_status().unwrap().xyz_new_data {
 //!         let data = sensor.accel_data().unwrap();
