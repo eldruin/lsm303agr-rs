9cac4e723ba9793a62487adc9f7ced87ea53e53e
Markus Reiter <me@reitermark.us>
2022-08-08 07:27:24 +0200

    Simplify and extend temperature tests.

--- tests/temperature_sensor.rs
+++ tests/temperature_sensor.rs
@@ -43,7 +43,7 @@ fn can_read_temperature_has_no_new_data() {
 }
 
 #[test]
-fn can_read_raw_temperature_data() {
+fn can_read_temperature_i2c() {
     let mut sensor = new_i2c(&[
         I2cTrans::write(
             ACCEL_ADDR,
@@ -52,41 +52,22 @@ fn can_read_raw_temperature_data() {
         I2cTrans::write_read(
             ACCEL_ADDR,
             vec![Register::OUT_TEMP_L_A | 0x80],
-            vec![0x10, 0x20],
+            vec![0xb3, 0xe2],
         ),
     ]);
 
     sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
     let data = sensor.temperature().unwrap();
-    assert_eq!(data.unscaled(), 0x2010);
+
+    assert_eq!(data.raw(), 0xe2b3);
+    assert_eq!(data.unscaled(), -7501);
+    assert_eq!((data.degrees_celsius() * 10.0).round() / 10.0, -4.3);
+
     destroy_i2c(sensor);
 }
 
 #[test]
-fn can_read_celsius_temperature_data() {
-    let mut sensor = new_i2c(&[
-        I2cTrans::write(
-            ACCEL_ADDR,
-            vec![Register::CTRL_REG1_A, DEFAULT_CTRL_REG1_A | HZ50],
-        ),
-        I2cTrans::write_read(
-            ACCEL_ADDR,
-            vec![Register::OUT_TEMP_L_A | 0x80],
-            vec![0x10, 0x20],
-        ),
-    ]);
-
-    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
-    let data = sensor.temperature().unwrap();
-    assert_eq!(
-        data.degrees_celsius().round() as i32,
-        ((0x2010 as f64 / 256.0) + 25.0).round() as i32
-    );
-    destroy_i2c(sensor);
-}
-
-#[test]
-fn can_read_raw_temperature_data_spi() {
+fn can_read_temperature_spi() {
     let mut sensor = new_spi_accel(
         &[
             SpiTrans::write(vec![Register::CTRL_REG1_A, DEFAULT_CTRL_REG1_A | HZ50]),
@@ -100,28 +81,10 @@ fn can_read_raw_temperature_data_spi() {
 
     sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
     let data = sensor.temperature().unwrap();
-    assert_eq!(data.unscaled(), 0x2010);
-    destroy_spi(sensor);
-}
 
-#[test]
-fn can_read_celsius_temperature_data_spi() {
-    let mut sensor = new_spi_accel(
-        &[
-            SpiTrans::write(vec![Register::CTRL_REG1_A, DEFAULT_CTRL_REG1_A | HZ50]),
-            SpiTrans::transfer(
-                vec![Register::OUT_TEMP_L_A | BF::SPI_RW | BF::SPI_MS, 0, 0],
-                vec![0, 0x10, 0x20],
-            ),
-        ],
-        default_cs_n(2),
-    );
+    assert_eq!(data.raw(), 0x2010);
+    assert_eq!(data.unscaled(), 8208);
+    assert_eq!((data.degrees_celsius() * 10.0).round() / 10.0, 57.1);
 
-    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
-    let data = sensor.temperature().unwrap();
-    assert_eq!(
-        data.degrees_celsius().round() as i32,
-        ((0x2010 as f64 / 256.0) + 25.0).round() as i32
-    );
     destroy_spi(sensor);
 }
