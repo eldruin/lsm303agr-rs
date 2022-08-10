be17045b3c0fcee5d744940e43fb8e9d56fe9d77
Diego Barrios Romero <eldruin@gmail.com>
2022-08-10 09:33:30 +0200

    Rename method device ID is_correct

--- tests/integration.rs
+++ tests/integration.rs
@@ -78,7 +78,7 @@ fn i2c_mag_id_is_correct() {
     let id = sensor.magnetometer_id().unwrap();
 
     assert_eq!(id.raw(), mag_id);
-    assert!(id.is_whoami());
+    assert!(id.is_correct());
 
     destroy_i2c(sensor);
 }
